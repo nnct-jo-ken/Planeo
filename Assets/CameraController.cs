525199362c5f9dd5e31554577348cff4c9005380
kash <krswshnt@gmail.com>
2015-07-29 03:28:08 +0900

    [add] add Planeo-base
    Planeo-base on unity

---
+++ Assets/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class CameraController : MonoBehaviour {
+
+	public float rotateSpeed = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Rotate
+		if (Input.GetButton ("CameraLeftRotate")) {
+			transform.Rotate(0, rotateSpeed, 0, Space.Self);
+		}
+		if (Input.GetButton ("CameraRightRotate")) {
+			transform.Rotate(0, -rotateSpeed, 0, Space.Self);
+		}
+
+
+	}
+}

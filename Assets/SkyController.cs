e00c8cb81439e4ac8c7d8c852169210943954898
kash <krswshnt@gmail.com>
2015-07-29 18:48:40 +0900

    [add] 星描画のサンプル
    星(プリミティブオブジェクトの球)を作成し、移動をさせるスクリプトの追加

---
+++ Assets/SkyController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject star = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		star.transform.position = new Vector3 (0, 1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

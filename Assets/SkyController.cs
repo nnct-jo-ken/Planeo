e9b2831671192e31d1c25a8472a10a088d31df16
kash <krswshnt@gmail.com>
2015-07-31 08:19:47 +0900

    [update] Starオブジェクトの親子関係の追加
    StarオブジェクトがSkyオブジェクトの子オブジェクトになるように修正

--- Assets/SkyController.cs
+++ Assets/SkyController.cs
@@ -3,14 +3,24 @@ using System.Collections;
 
 public class SkyController : MonoBehaviour {
 
+	private GameObject sky;
+
+
+
 	// Use this for initialization
 	void Start () {
+		sky = GameObject.Find ("Sky");
 		GameObject star = GameObject.CreatePrimitive (PrimitiveType.Sphere);
-		star.transform.position = new Vector3 (0, 1, 0);
+		star.transform.position = new Vector3 (25, 20, 20);
+		star.transform.parent = sky.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+}
+
+public class Star : MonoBehaviour {
+
 }

24067192aaa45f484444ce0debfc32a4d0561c0a
kash <krswshnt@gmail.com>
2015-08-02 04:19:06 +0900

    [add] カーソルの追加
    カーソルの管理スクリプトの追加

---
+++ Assets/CursorController.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Cursor control script
+ * 1.show Cursor Texture
+ * 2.move center
+ * 
+ * select enable or disable
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class CursorController : MonoBehaviour {
+
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter(){
+		Cursor.SetCursor (cursorTexture, hotSpot, cursorMode);
+	}
+
+	void OnMouseExit(){
+		Cursor.SetCursor (null, Vector2.zero, cursorMode);
+	}
+}

a421fd4585eeef0d482382039e3d5192f9d89ba1
kash <krswshnt@gmail.com>
2015-08-04 19:46:27 +0900

    [add] add camera zooming script
    カメラのズームを管理するスクリプトを追加

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+	public float zoomSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		ZoomCamera ();	
+	}
+
+	private void ZoomCamera () {
+		if (Input.GetButton ("CameraZoomIn")) {
+			transform.Translate (0, 0, zoomSpeed * Time.deltaTime, Space.Self);
+		}
+		if (Input.GetButton ("CameraZoomOut")) {
+			transform.Translate (0, 0, -zoomSpeed * Time.deltaTime, Space.Self);
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			print (transform.position.x);
+			print (transform.position.y);
+			print (transform.position.z);
+		}
+	}
+}

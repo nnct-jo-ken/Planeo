3717a535188d9de73e0b063b0a64ccefd16c9b0e
kash <krswshnt@gmail.com>
2015-08-08 11:45:48 +0900

    [update] カーソルの表示
    カーソルの非表示を可能に

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -1,9 +1,6 @@
 ﻿/*
  * Cursor control script
- * 1.show Cursor Texture
- * 2.move center
- * 
- * select enable or disable
+ *    select enable or disable
  */
 
 using UnityEngine;
@@ -11,26 +8,21 @@ using System.Collections;
 
 public class CursorController : MonoBehaviour {
 
-	public Texture2D cursorTexture;
-	public CursorMode cursorMode = CursorMode.Auto;
-	public Vector2 hotSpot = Vector2.zero;
-
+	private GameObject cursor;
 
 	// Use this for initialization
 	void Start () {
-		OnMouseEnter ();
+		cursor = transform.FindChild("Cursor").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-	void OnMouseEnter(){
-		Cursor.SetCursor (cursorTexture, hotSpot, cursorMode);
-	}
-
-	void OnMouseExit(){
-		Cursor.SetCursor (null, Vector2.zero, cursorMode);
+		if (Input.GetButtonDown("ShowCursor")) {
+			if (cursor.activeSelf == false) {
+				cursor.SetActive (true);
+			} else if (cursor.activeSelf == true) {
+				cursor.SetActive (false);
+			}
+		}
 	}
 }

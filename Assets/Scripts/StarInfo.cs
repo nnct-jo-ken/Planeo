98a4c2b42e272ad8bc4bbb8d434dfda71924a280
kash <krswshnt@gmail.com>
2015-08-12 16:28:58 +0900

    [update] member of StarInfo(Script)
    各種管理用のメンバーの追加

--- Assets/Scripts/StarInfo.cs
+++ Assets/Scripts/StarInfo.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class StarInfo : MonoBehaviour {
 
+	public int catalogNumber;
+	public Vector3 starPosition;  // xyz座標上の位置
+	public float ra;
+	public float dec;
+
+	// 特になければ空
+	public string japaneseName;
+	public string englishName;  // GameObjectの名前にも使用
+	public string explainText;
+
+	public float magnitude;
+
+	private Vector3 starColor;
+
+
+
+	// コンストラクタ
+	public StarInfo() {
+	}
+	// 説明用のGUIメソッド
+	// スケーリング用(等級によってサイズ変更？？)
+	// 
+
+
+
+
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	//void Start () {}
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	//void Update () {}
 }

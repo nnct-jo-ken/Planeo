569c21b5b4fcfe8ce57913b83c2296cf50f184f7
kash <krswshnt@gmail.com>
2015-08-13 16:48:27 +0900

    [add] RayController(script)
    説明文を表示するためのRayを管理するスクリプトを追加

---
+++ Assets/Scripts/RayController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayController : MonoBehaviour {
+	RaycastHit hitInfo;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	private void ShowInfomation() {
+		GameObject camera = GameObject.Find("CameraControl/Main Camera");
+		// 表示されていたらオブジェクトを破壊してから表示
+		// 何も当たらない場合は破壊のみ
+		if (Input.GetButtonDown ("ShowInfomation")) {
+			Physics.Raycast (Vector3.zero, camera.transform.position, out hitInfo);
+		}
+	}
+}

1bbba70bc9539ca16325f0f95c9ad97f6bc7b08e
kash <krswshnt@gmail.com>
2015-08-21 01:58:48 +0900

    [fix] Rayの向きを修正
    カメラの前にRay用のオブジェクトを置き、カメラとRay用のオブジェクト間のベクトルでRayの向きを決定

--- Assets/Scripts/RayController.cs
+++ Assets/Scripts/RayController.cs
@@ -4,17 +4,18 @@ using System.Collections;
 public class RayController : MonoBehaviour {
 	RaycastHit hitInfo;
 	GameObject camera;
-
+	GameObject rayControl;
+	StarInfo hitStarInfo;
 
 	// Use this for initialization
 	void Start () {
 		camera = GameObject.Find("CameraControl/Main Camera");
-
+		rayControl = GameObject.Find ("CameraControl/Main Camera/RayControl");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		ShowInfomation ();
 	}
 
 
@@ -22,7 +23,14 @@ public class RayController : MonoBehaviour {
 		// 表示されていたらオブジェクトを破壊してから表示
 		// 何も当たらない場合は破壊のみ
 		if (Input.GetButtonDown ("ShowInfomation")) {
-			Physics.Raycast (Vector3.zero, camera.transform.position, out hitInfo);
+			if (Physics.Raycast (camera.transform.position, rayControl.transform.position, out hitInfo)) {
+				hitStarInfo = hitInfo.transform.gameObject.GetComponent<StarInfo> ();
+				Debug.Log (hitStarInfo.catalogNumber);
+				Debug.DrawLine (camera.transform.position, hitInfo.point);
+			} else {
+				Debug.Log ("non");
+				Debug.Log (camera.transform.position);
+			}
 		}
 	}
 }

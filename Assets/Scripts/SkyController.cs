ce6da77b429d16a51f0be67b0d7e9695a506ef24
kash <krswshnt@gmail.com>
2015-09-20 23:03:09 +0900

    [clean] Rename axis → rotationAxis

--- Assets/Scripts/SkyController.cs
+++ Assets/Scripts/SkyController.cs
@@ -11,17 +11,17 @@ public class SkyController : MonoBehaviour {
 
 	float latitude;           // 緯度
 	float longitude;          // 経度
-	float axis;               // 自転軸
+	float rotationAxis;               // 自転軸
 	public int rotationSpeed; // 自転速度
 
 	// Use this for initialization
 	void Start () {
 		latitude = CommonConstants.LatLng.HOCTO_Lat;
 		longitude = CommonConstants.LatLng.HOCTO_Lng;
-		axis = CommonConstants.General.EARTH_AXIS;
+		rotationAxis = CommonConstants.General.EARTH_AXIS;
 		rotationSpeed = 15;
 
-		transform.Rotate (0, 0, axis, Space.World);
+		transform.Rotate (0, 0, rotationAxis, Space.World);
 	}
 	
 	// Update is called once per frame

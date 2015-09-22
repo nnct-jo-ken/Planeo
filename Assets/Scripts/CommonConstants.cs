432e545e1494d088b9a04c777e83f013ab4aa2f7
kash <krswshnt@gmail.com>
2015-09-22 13:39:53 +0900

    [add] planet data(semi-major axis, eccentricity)

--- Assets/Scripts/CommonConstants.cs
+++ Assets/Scripts/CommonConstants.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CommonConstants : MonoBehaviour {
+public class CommonConstants {
 
 	public class General {
 		public static readonly float EARTH_AXIS = 23.44f;    // 理科年表より
@@ -42,5 +42,39 @@ public class CommonConstants : MonoBehaviour {
 		public static readonly float BRAZIL_Lng = -47.864154f;
 	}
 
-
+	public class Planet {
+		// 基本的に理科年表(H.27)より
+		public class Mercury {
+			public static readonly float SEMI_MEJOR_AXIS_AU = 0.3871f;
+			public static readonly float ECCENTRICITY = 0.2056f;
+		}
+		public class Venus {
+			public static readonly float SEMI_MEJOR_AXIS_AU = 0.7233f;
+			public static readonly float ECCENTRICITY = 0.0068f;
+		}
+		public class Earth {
+			public static readonly float SEMI_MEJOR_AXIS_AU = 1.0f;
+			public static readonly float ECCENTRICITY = 0.0167f;
+		}
+		public class Mars {
+			public static readonly float SEMI_MEJOR_AXIS_AU = 1.5237f;
+			public static readonly float ECCENTRICITY = 0.0934f;
+		}
+		public class Jupiter {
+			public static readonly float SEMI_MEJOR_AXIS_AU = 5.2026f;
+			public static readonly float ECCENTRICITY = 0.0485f;
+		}
+		public class Saturn {
+			public static readonly float SEMI_MEJOR_AXIS_AU = 9.5549f;
+			public static readonly float ECCENTRICITY = 0.0555f;
+		}
+		public class Uranus {
+			public static readonly float SEMI_MEJOR_AXIS_AU = 19.2184f;
+			public static readonly float ECCENTRICITY = 0.0463f;
+		}
+		public class Neptune {
+			public static readonly float SEMI_MEJOR_AXIS_AU = 30.1104f;
+			public static readonly float ECCENTRICITY = 0.0090f;
+		}
+	}
 }

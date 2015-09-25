35fb86145b4c34baf42f5eb4ef0c519630d2fbb7
kash <krswshnt@gmail.com>
2015-09-25 15:24:44 +0900

    [add] Planet Data(epoch mean anomaly)

--- Assets/Scripts/CommonConstants.cs
+++ Assets/Scripts/CommonConstants.cs
@@ -45,44 +45,52 @@ public class CommonConstants {
 	public class Planet {
 		// 基本的に理科年表(H.27)より
 		public class Mercury {
-			public static readonly float SEMI_MEJOR_AXIS_AU = 0.3871f;
-			public static readonly float ECCENTRICITY = 0.2056f;
-			public static readonly float SIDEREAL_PERIOD = 0.24085;
+			public static readonly float SEMI_MEJOR_AXIS_AU = 0.3871f;     // 軌道長半径 (Au)
+			public static readonly float ECCENTRICITY = 0.2056f;           // 離心率
+			public static readonly float SIDEREAL_PERIOD_JY = 0.24085f;     // 対恒星公転周期 (Julian Year)
+			public static readonly float EPOCH_MEAN_ANOMALY = 278.992f;            // 元期平均近点離角
 		}
 		public class Venus {
 			public static readonly float SEMI_MEJOR_AXIS_AU = 0.7233f;
 			public static readonly float ECCENTRICITY = 0.0068f;
-			public static readonly float SIDEREAL_PERIOD = 0.61520;
+			public static readonly float SIDEREAL_PERIOD_JY = 0.61520f;
+			public static readonly float EPOCH_MEAN_ANOMALY = 111.264f;
 		}
 		public class Earth {
 			public static readonly float SEMI_MEJOR_AXIS_AU = 1.0f;
 			public static readonly float ECCENTRICITY = 0.0167f;
-			public static readonly float SIDEREAL_PERIOD = 1.00002;
+			public static readonly float SIDEREAL_PERIOD_JY = 1.00002f;
+			public static readonly float EPOCH_MEAN_ANOMALY = 171.590f;
 		}
 		public class Mars {
 			public static readonly float SEMI_MEJOR_AXIS_AU = 1.5237f;
 			public static readonly float ECCENTRICITY = 0.0934f;
-			public static readonly float SIDEREAL_PERIOD = 1.88085;
+			public static readonly float SIDEREAL_PERIOD_JY = 1.88085f;
+			public static readonly float EPOCH_MEAN_ANOMALY = 102.972f;
 		}
 		public class Jupiter {
 			public static readonly float SEMI_MEJOR_AXIS_AU = 5.2026f;
 			public static readonly float ECCENTRICITY = 0.0485f;
-			public static readonly float SIDEREAL_PERIOD = 11.8620;
+			public static readonly float SIDEREAL_PERIOD_JY = 11.8620f;
+			public static readonly float EPOCH_MEAN_ANOMALY = 129.909f;
 		}
 		public class Saturn {
 			public static readonly float SEMI_MEJOR_AXIS_AU = 9.5549f;
 			public static readonly float ECCENTRICITY = 0.0555f;
-			public static readonly float SIDEREAL_PERIOD = 29.4572;
+			public static readonly float SIDEREAL_PERIOD_JY = 29.4572f;
+			public static readonly float EPOCH_MEAN_ANOMALY = 146.164f;
 		}
 		public class Uranus {
 			public static readonly float SEMI_MEJOR_AXIS_AU = 19.2184f;
 			public static readonly float ECCENTRICITY = 0.0463f;
-			public static readonly float SIDEREAL_PERIOD = 84.0205;
+			public static readonly float SIDEREAL_PERIOD_JY = 84.0205f;
+			public static readonly float EPOCH_MEAN_ANOMALY = 207.380f;
 		}
 		public class Neptune {
 			public static readonly float SEMI_MEJOR_AXIS_AU = 30.1104f;
 			public static readonly float ECCENTRICITY = 0.0090f;
-			public static readonly float SIDEREAL_PERIOD = 164.7700;
+			public static readonly float SIDEREAL_PERIOD_JY = 164.7700f;
+			public static readonly float EPOCH_MEAN_ANOMALY = 290.052f;
 		}
 	}
 }

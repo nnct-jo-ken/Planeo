b95fd90d8eb14f78a77e76beef906ddd2a64f479
kash <krswshnt@gmail.com>
2015-09-26 07:38:29 +0900

    [add] Planet Enum
    惑星の列挙体を定義

--- Assets/Scripts/PlanetController.cs
+++ Assets/Scripts/PlanetController.cs
@@ -1,5 +1,20 @@
 ﻿using UnityEngine;
+﻿/* 惑星管理用スクリプト
+ * 
+ */
+using UnityEngine;
 using System.Collections;
+
+public enum Planet {
+	Mercury,   // 水星
+	Venus,     // 金星
+	Earth,     // 地球
+	Mars,      // 火星
+	Jupiter,   // 木星
+	Saturn,    // 土星
+	Uranus,    // 天王星
+	Neptune,   // 海王星
+}
 
 public class PlanetController : MonoBehaviour {
 

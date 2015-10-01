9848b375b0411e719e723db71a5b092d009a86aa
kash <krswshnt@gmail.com>
2015-10-02 05:19:31 +0900

    [update] GameManagerでモードの管理

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	private static bool isMode = true;     // 1:地球 0:宇宙
+	private static bool isVisual = true;   // 1:可視光,0:不可視光
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,14 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		ChangeVisual ();
+	}
+
+	void ChangeVisual() {
+		if (isMode == true) {
+			if (Input.GetButtonUp ("Visual")) {
+				isVisual = !isVisual;
+			}
+		}
 	}
 }

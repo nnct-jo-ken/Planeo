053ddf5ad66a79d8f662ebb81762f7aa4bcf3618
kash <krswshnt@gmail.com>
2015-10-02 05:20:09 +0900

    [update] 星のスケールの変更

--- Assets/Scripts/StarInfo.cs
+++ Assets/Scripts/StarInfo.cs
@@ -20,11 +20,13 @@ public class StarInfo : MonoBehaviour {
 	// スケーリング用(等級によってサイズ変更)
 	public void Scaling() {
 		if (magnitude <= 1.0f) {
-			transform.localScale = new Vector3 (2.5f, 2.5f, 2.5f);
+			transform.localScale = new Vector3 (3.0f, 3.0f, 3.0f);
 		} else if (magnitude <= 2.0f) {
 			transform.localScale = new Vector3 (2.0f, 2.0f, 2.0f);
 		} else if (magnitude <= 4.0f) {
-			transform.localScale = new Vector3 (1.5f, 1.5f, 1.5f);
+			transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+		} else if (magnitude <= 6.5f) {
+			transform.localScale = new Vector3 (0.3f, 0.3f, 0.3f);
 		}
 	}
 

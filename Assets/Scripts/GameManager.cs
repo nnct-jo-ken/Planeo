20072301f414cb6f714c33c74fffa97187675d7c
kash <krswshnt@gmail.com>
2015-10-03 18:09:34 +0900

    [update] pravate → public

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	private static bool isMode = true;     // 1:地球 0:宇宙
-	private static bool isVisual = true;   // 1:可視光,0:不可視光
+	public static bool isMode = true;     // 1:地球 0:宇宙
+	public static bool isVisual = true;   // 1:可視光,0:不可視光
 
 	// Use this for initialization
 	void Start () {

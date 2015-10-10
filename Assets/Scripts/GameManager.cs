29d77985bd5465c328a8205845ae0c6d0836bebd
kash <krswshnt@gmail.com>
2015-10-11 02:45:35 +0900

    [update] 不可視光モードへの切り替えの実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ public class GameManager : MonoBehaviour {
 
 	public bool isMode = true;     // 1:地球 0:宇宙
 	public bool isVisual = true;   // 1:可視光,0:不可視光
+	public GameObject visualSkyStar;
+	public GameObject visualSkyPlanet;
+	public GameObject infraredSky;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +23,16 @@ public class GameManager : MonoBehaviour {
 		if (isMode == true) {
 			if (Input.GetButtonUp ("Visual")) {
 				isVisual = !isVisual;
+				if (isVisual == true) {
+					visualSkyStar.SetActive (true);
+					visualSkyPlanet.SetActive (true);
+					infraredSky.SetActive (false);
+				} else if (isVisual == false) {
+					visualSkyStar.SetActive (false);
+					visualSkyPlanet.SetActive (false);
+					infraredSky.SetActive (true);
+				}
+
 			}
 		}
 	}
